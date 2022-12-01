0089277fdf22ab8f50f176557d906b2cbcb3b5ee
isha056 <115340328+isha056@users.noreply.github.com>
2022-12-01 20:19:06 +0530

    Create variable.py

---
+++ week1/variable.py
@@ -0,0 +1,13 @@
+x=5
+print(x)
+name="Isha"
+print(name)
+# use first letter ---- letter,_
+_y=7
+print(_y)
+#snake case writing --- My_name_is
+#camel case writing --- userOneName
+# 1name="Simran" -- error
+# nsme1="Simran" -- no error
+name,age="Isha","17"
+print("hello" + name + " you age is " + age)
