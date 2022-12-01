6e04bbc6719a1f53a546f6f297a082de7c37ca35
isha056 <115340328+isha056@users.noreply.github.com>
2022-12-01 20:00:43 +0530

    Create inkeyboard.py

---
+++ week1/inkeyboard.py
@@ -0,0 +1,12 @@
+x="Isha"
+if "a" in x:
+    print("a is present in x")
+else:
+    print(" a is not present in x")
+
+# check empty or not
+x="Isha"
+if x: # true if string is notg empty
+    print("string is not empty")
+else:
+    print("string is empty")
